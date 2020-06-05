0b53db7ba1dbe9f2672957102afa150420712bae
Ubuntu <ubuntu@ip-172-31-28-95.us-west-1.compute.internal>
2020-06-05 22:00:14 +0000

    added roc curve using validation split and added roc curve plot funtion to helpers.py

--- src/.ipynb_checkpoints/helpers-checkpoint.py
+++ src/.ipynb_checkpoints/helpers-checkpoint.py
@@ -5,6 +5,8 @@ from nltk import pos_tag
 from nltk.corpus import wordnet, stopwords
 from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
+import pandas as pd
+import matplotlib.pyplot as plt
 
 sw = stopwords.words('english') # english stopwords list
 pt = string.punctuation # punct list
@@ -32,3 +34,32 @@ def preprocessor(doc):
     pos_tagged_text = pos_tag(doc.split())
     lemmatizer = WordNetLemmatizer()
     return " ".join([lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
+
+def calculate_threshold_values(prob, y):
+    '''
+    Build dataframe of the various confusion-matrix ratios by threshold
+    from a list of predicted probabilities and actual y values
+    '''
+    df = pd.DataFrame({'prob': prob, 'y': y})
+    df.sort_values('prob', inplace=True) # kind of the biggest deal here in terms of making this ROC curve possible
+    
+    actual_p = df.y.sum()
+    actual_n = df.shape[0] - df.y.sum()
+
+    df['tn'] = (df.y == 0).cumsum()
+    df['fn'] = df.y.cumsum()
+    df['fp'] = actual_n - df.tn
+    df['tp'] = actual_p - df.fn
+
+    df['fpr'] = df.fp/(df.fp + df.tn)
+    df['tpr'] = df.tp/(df.tp + df.fn)
+    df['precision'] = df.tp/(df.tp + df.fp)
+    df = df.reset_index(drop=True)
+    return df
+
+def plot_roc(ax, df, model_name):
+    ax.plot([1]+list(df.fpr), [1]+list(df.tpr), label=model_name)
+    ax.set_xlabel('False Positive Rate', fontsize = 15)
+    ax.set_ylabel('True Positive Rate', fontsize = 15)
+    ax.set_title('Model Comparison ROC Curve', fontsize = 20)
+#     ax.legend()
